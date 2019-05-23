9fd76bcb1f72e073e931d895bf2633655cae7bb4
Ray Xu <megrxu@gmail.com>
2019-05-23 14:43:07 +0800

    [update] PRESENT implemented.

--- eva-crypto/src/present.rs
+++ eva-crypto/src/present.rs
@@ -1,7 +1,6 @@
 use super::generic::{
     create_u8x4x4, expand_bits, restore_data, transpose, u8x4x4, Ops, Permutation,
 };
-use bitreader::BitReader;
 
 type PREstate = u8x4x4;
 
@@ -20,37 +19,79 @@ impl PRESENT {
             round_keys: round_keys,
         }
     }
-}
 
+    /// Encrypt a block.
+    pub fn encrypt(self, data: &[u8]) -> Vec<u8> {
+        let rounds = 32;
+        let mut state = create_u8x4x4(data);
+        for i in 0..rounds - 1 {
+            state = add_round_key(&state, &self.round_keys[i]);
+            state = sbox_layer(&state);
+            state = p_layer(&state, &PBOX);
+        }
+        state = add_round_key(&state, &self.round_keys[rounds - 1]);
+        state.concat()
+    }
+
+    /// Decrypt a block.
+    pub fn decrypt(self, data: &[u8]) -> Vec<u8> {
+        let rounds = 32;
+        let mut state = create_u8x4x4(data);
+        for i in (1..rounds).rev() {
+            state = add_round_key(&state, &self.round_keys[i]);
+            state = p_layer(&state, &RPBOX);
+            state = inv_sbox_layer(&state);
+        }
+        state = add_round_key(&state, &self.round_keys[0]);
+        state.concat()
+    }
+}
+fn add_round_key(state: &PREstate, round_key: &PREstate) -> PREstate {
+    state.xor(&transpose(&round_key))
+}
+fn sbox_layer(state: &PREstate) -> PREstate {
+    state.sub_sbox(&SBOX)
+}
+fn inv_sbox_layer(state: &PREstate) -> PREstate {
+    state.sub_sbox(&RSBOX)
+}
+fn p_layer(state: &PREstate, pbox: &[u8]) -> PREstate {
+    let bits = expand_bits(&state.concat().to_vec());
+    let mut p_bits: Vec<bool> = vec![true; 64];
+    for i in 0..64 {
+        p_bits[i] = bits[pbox[i] as usize];
+    }
+    create_u8x4x4(&restore_data(&p_bits)[0..16])
+}
 fn key_expansion(key: Vec<u8>, round_keys: &mut [PREstate]) {
     let keysize = key.len() * 4;
     let rounds = round_keys.len();
     let mut k_register = expand_bits(&key);
     match keysize {
-        80 => {
-            for i in 0..rounds {
-                round_keys[i] = create_u8x4x4(&restore_data(&k_register)[0..16]);
-                k_register[..].rotate_left(61);
-                let mut buffer = restore_data(&k_register);
-                buffer[0] = SBOX[buffer[0] as usize];
-                buffer[15] ^= (i + 1 >> 1) as u8;
-                buffer[16] ^= (i + 1 << 3 & 0xf) as u8;
-                k_register = expand_bits(&buffer);
-            }
-        }
-        128 => {
-            for i in 0..rounds {
-                round_keys[i] = create_u8x4x4(&restore_data(&k_register)[0..16]);
-                k_register[..].rotate_left(61);
-                let mut buffer = restore_data(&k_register);
-                buffer[0] = SBOX[buffer[0] as usize];
-                buffer[1] = SBOX[buffer[1] as usize];
-                buffer[0] ^= (i + 1 >> 1) as u8;
-                buffer[1] ^= (i + 1 << 4 & 0xf) as u8;
-                k_register = expand_bits(&buffer);
-            }
-        }
+        80 | 128 => (),
         _ => panic!("Key length {} is not valid!", keysize),
+    }
+
+    for i in 0..rounds {
+        round_keys[i] = create_u8x4x4(&restore_data(&k_register)[0..16]);
+        // rotate left 61 bits
+        k_register[..].rotate_left(61);
+        let mut buffer = restore_data(&k_register);
+        if keysize == 80 {
+            // Sbox
+            buffer[0] = SBOX[buffer[0] as usize];
+            //  XOR with the round counter
+            buffer[15] ^= (i + 1 >> 1) as u8;
+            buffer[16] ^= (i + 1 << 3 & 0xf) as u8;
+        } else {
+            // Sbox
+            buffer[0] = SBOX[buffer[0] as usize];
+            buffer[1] = SBOX[buffer[1] as usize];
+            //  XOR with the round counter
+            buffer[15] ^= (i + 1 >> 2) as u8;
+            buffer[16] ^= (i + 1 << 2 & 0xf) as u8;
+        }
+        k_register = expand_bits(&buffer);
     }
 }
 
--- eva-crypto/tests/present_test.rs
+++ eva-crypto/tests/present_test.rs
@@ -8,3 +8,21 @@ fn test_key_expansion() {
         [0x6, 0xd, 0xa, 0xb, 0x3, 0x1, 0x7, 0x4, 0x4, 0xf, 0x4, 0x1, 0xd, 0x7, 0x0, 0x0]
     );
 }
+
+#[test]
+fn test_present_80() {
+    let key = [0u8; 20];
+    let plaintext = [0u8; 16];
+    let ciphertext = [5, 0xc, 7, 8, 5, 1, 0xb, 4, 7, 3, 2, 4, 9, 8, 2, 5];
+    assert_eq!(PRESENT::new(&key).encrypt(&plaintext), ciphertext);
+    assert_eq!(PRESENT::new(&key).decrypt(&ciphertext), plaintext);
+}
+
+#[test]
+fn test_present_128() {
+    let key = [0u8; 32];
+    let plaintext = [0u8; 16];
+    let ciphertext = [9, 7, 2, 0, 6, 0, 0xe, 0, 0xd, 2, 6, 0xa, 0xb, 0xa, 0x9, 0xf];
+    assert_eq!(PRESENT::new(&key).encrypt(&plaintext), ciphertext);
+    assert_eq!(PRESENT::new(&key).decrypt(&ciphertext), plaintext);
+}
