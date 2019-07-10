d3fa9f6f4f7b6436c29bf28115d0b947bbb1ed22
Ray Xu <megrxu@gmail.com>
2019-07-10 16:48:33 +0800

    [feature] fault injection

--- eva-crypto/examples/aes-pfa.rs
+++ eva-crypto/examples/aes-pfa.rs
@@ -20,7 +20,7 @@ fn main() -> io::Result<()> {
     let mut buffer = [0u8; 16];
     let mut f = File::open("examples/data/aes/out.bin")?;
     let mut stats = [[0u32; 256]; 16];
-    let count = 2000;
+    let count = 1500;
 
     for _ in 0..count {
         f.read(&mut buffer)?;
@@ -30,24 +30,37 @@ fn main() -> io::Result<()> {
     }
 
     // Round 10 Analysis
-    let mut kc = [0u8; 16];
-    let mut size = 1.0f32;
+    let mut kc: [Vec<u8>; 16] = Default::default();
     for i in 0..16 {
-        let mut tmp = 0;
         for byte in 0..0xff {
             if stats[i as usize][byte] == 0 {
-                kc[i] = byte as u8 ^ 0x63u8;
-                tmp += 1;
+                kc[i].push(byte as u8 ^ 0x63u8);
             }
         }
-        size *= tmp as f32;
     }
+    let size = kc.iter().fold(1, |res, x| res * x.len());
 
-    println!("Target: {:x?}", cipher.round_keys[10]);
-    println!("Result: {:x?}", create_u8x4x4(&kc));
-    println!("Residue Entropy: {}", size.log2());
+    println!("Target: {:2x?}", cipher.round_keys[10]);
+    for idx in 0..size {
+        println!(
+            "Result: {:2x?}",
+            create_u8x4x4(&cproduct(&kc, idx, size)[0..16])
+        );
+    }
+    println!("Residue Entropy: {}", (size as f32).log2());
 
     Ok(())
+}
+
+fn cproduct(kc: &[Vec<u8>; 16], mut idx: usize, mut size: usize) -> Vec<u8> {
+    let len_arr: Vec<usize> = kc.iter().map(|x| x.len()).collect();
+    let mut idx_arr = [0usize; 16];
+    for i in (0..16).rev() {
+        size /= len_arr[i];
+        idx_arr[i] = idx / size;
+        idx -= idx_arr[i] * size;
+    }
+    kc.iter().enumerate().map(|(i, x)| x[idx_arr[i]]).collect()
 }
 
 fn generate_data(key: [u8; 16]) -> io::Result<()> {
